52daea15183364364a070d7cbaa16bec79c80262
Muhammad Junaid Butt <m.junaid.ft@gmail.com>
2017-09-21 13:39:24 +0500

    - Fixed the following issue: https://trello.com/c/pIe7X8Ch/217-4012-description-should-increase-to-1000-characters

--- OmnEvent/Classes/Controller/OMPostEventViewController.m
+++ OmnEvent/Classes/Controller/OMPostEventViewController.m
@@ -123,6 +123,8 @@
     tapGestureForBg.numberOfTapsRequired = 1;
     [tapGestureForBg setDelegate:self];
     [imageViewForPostImage addGestureRecognizer:tapGestureForBg];
+    
+    lblForCount.adjustsFontSizeToFitWidth = YES;
     lblForCount.text = [NSString stringWithFormat:@"%d", MAX_DESCRIPTION_LIMIT];
     countryLatLong = @"Unknown";
     
